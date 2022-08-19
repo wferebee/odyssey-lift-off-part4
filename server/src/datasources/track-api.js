e98acf09b5a2866330cbeeaa365226dbf0042957
William Ferebee <williampferebee@gmail.com>
2022-08-19 13:51:45 -0400

    Added a mutation

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -23,9 +23,9 @@ class TrackAPI extends RESTDataSource {
     return this.get(`track/${trackId}/modules`);
   }
 
-  getModule(moduleId) {
-    return this.get(`module/${moduleId}`);
-  }
+  incrementTrackViews(trackId) {
+    return this.patch(`track/${trackId}/numberOfViews`);
+ }
 }
 
 module.exports = TrackAPI;
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,25 +4,38 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
-
     // get a single track by ID, for the track page
-    track: (_, { id }, { dataSources }) => {
+    track: (_, {id}, {dataSources}) => {
       return dataSources.trackAPI.getTrack(id);
     },
-
-    // get a single module by ID, for the module detail page
-    module: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI.getModule(id);
+  },
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try{
+         const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null
+        };
+      }
     },
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
-    modules: ({ id }, _, { dataSources }) => {
+    modules: ({id}, _, {dataSources}) => {
       return dataSources.trackAPI.getTrackModules(id);
-    },
+    }
   },
 };
 
