14bb3a545831d4e1717a363b0523ab0a79bd5cb3
William Ferebee <williampferebee@gmail.com>
2022-08-19 13:55:17 -0400

    changes Schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,10 +4,11 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-    "Fetch a specific track, provided a track's ID"
-    track(id: ID!): Track!
-    "Fetch a specific module, provided a module's ID"
-    module(id: ID!): Module!
+    track(id: ID!): Track
+  }
+
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse! 
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,11 +20,11 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
+    "The track's approximate length to complete, in minues"
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
-    "The track's complete description, can be in markdown format"
+    "The track's complete description, can be in Markdown format"
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
@@ -43,14 +44,21 @@ const typeDefs = gql`
   "A Module is a single unit of teaching. Multiple Modules compose a Track"
   type Module {
     id: ID!
-    "The module's title"
+    "The Module's title"
     title: String!
-    "The module's length in minutes"
+    "The Module's length in minutes"
     length: Int
-    "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
-    content: String
-    "The module's video url, for video-based modules"
-    videoUrl: String
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
   }
 `;
 
